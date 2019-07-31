8835e20bde541cfd8f50aa354e7f1b7228d1f29c
Philip <p208p2002@gmail.com>
2019-07-31 07:43:01 +0000

    no message

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 # Judge of sentence positive or negative
 # 檔案描述
- 檔名 | 說明 |
------|-----|
-mid_correction.py|中心校正|
+File name            |descripition       |
+---------------------|-------------------|
+mid_correction.py    |中心字校正          |
+main.py              |主程式              |
--- main.py
+++ main.py
@@ -32,12 +32,29 @@ if __name__ == "__main__":
     middleOfNegative = middleWord[1]
     print(middleOfPostive,middleOfNegative)
 
+    # 正面詞集
+    with open('dict/positive_words.txt','r',encoding='utf-8') as f:
+        data = f.read()
+    postiveData = data.split()
+
+    # 反面詞集
+    with open('dict/negative_words.txt','r',encoding='utf-8') as f:
+        data = f.read()
+    negativeData = data.split()
+
     # 測試檔案
     with open('testcases/test.txt','r',encoding='utf-8') as f:
-        testCases = f.read()    
-    testCases = testCases.split()
-
+        testCases = f.read()
+    testCases = testCases.split('\n')
+    testCasesCount = len(testCases)
+    passCount = 0
     for tc in testCases:
+        tc = tc.split(' ')
+        try:
+            ans = int(tc[1])
+        except:
+            ans = 0
+        tc = tc[0]
         words = jieba.cut(tc, cut_all=False)
         splitData = []
         splitDataVal_postive = []
@@ -45,22 +62,42 @@ if __name__ == "__main__":
         for word in words:
             if not word in stopword_set:
                 splitData.append(word)
-                pVal = compareSimilar(middleOfPostive,word)
-                splitDataVal_postive.append(pVal)
+                pVal = compareSimilar(middleOfPostive,word)                
                 nVal = compareSimilar(middleOfNegative,word)
+                
+                # 加權
+                if word in postiveData:
+                    pVal = pVal*1.5
+                elif word in negativeData:
+                    nVal = nVal*1.5
+
+                splitDataVal_postive.append(pVal)
                 splitDataVal_negative.append(nVal)
         
-        print(tc)
+        print(tc,ans)
         print(splitData)
         pSum = sum(splitDataVal_postive)*100
         nSum = sum(splitDataVal_negative)*100
         print("P:",pSum)
+        print(splitDataVal_postive)
         print("N:",nSum)
-        if((pSum+nSum)*0.08 >= abs(pSum-nSum)): #差距小於總分的8%
+        print(splitDataVal_negative)
+        if((pSum+nSum)*0.06 >= abs(pSum-nSum)): #差距小於總分的8%
             print("中立",pSum-nSum)
+            if(ans == 0):
+                passCount += 1
+                print("**pass**")
         elif(pSum>nSum):
             print("正面",pSum-nSum)
+            if(ans == 1):
+                passCount += 1
+                print("**pass**")
         else:
             print("反面",pSum-nSum)
+            if(ans == -1):
+                passCount += 1
+                print("**pass**")
         print("\n")
+    #
+    print('正確率',passCount,'/',testCasesCount,passCount/testCasesCount)
         
