422a74fb3d783fb3f0a376cb2b96290f1040534b
Philip <p208p2002@gmail.com>
2019-07-31 06:52:02 +0000

    no message

--- main.py
+++ main.py
@@ -2,6 +2,13 @@ import jieba
 from gensim.models import word2vec
 from gensim import models
 
+def compareSimilar(str_a,str_b):
+    try:
+        val = model.wv.similarity(str_a,str_b)
+    except:
+        val = 0.0
+    return val
+
 if __name__ == "__main__":
     # jieba字典
     jieba.set_dictionary('dict/dict.txt.big')
@@ -33,8 +40,27 @@ if __name__ == "__main__":
     for tc in testCases:
         words = jieba.cut(tc, cut_all=False)
         splitData = []
+        splitDataVal_postive = []
+        splitDataVal_negative = []
         for word in words:
             if not word in stopword_set:
                 splitData.append(word)
+                pVal = compareSimilar(middleOfPostive,word)
+                splitDataVal_postive.append(pVal)
+                nVal = compareSimilar(middleOfNegative,word)
+                splitDataVal_negative.append(nVal)
+        
+        print(tc)
         print(splitData)
+        pSum = sum(splitDataVal_postive)*100
+        nSum = sum(splitDataVal_negative)*100
+        print("P:",pSum)
+        print("N:",nSum)
+        if((pSum+nSum)*0.08 >= abs(pSum-nSum)): #差距小於總分的8%
+            print("中立",pSum-nSum)
+        elif(pSum>nSum):
+            print("正面",pSum-nSum)
+        else:
+            print("反面",pSum-nSum)
+        print("\n")
         
